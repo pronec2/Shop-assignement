c85fa3af092bfe9013b85f947f2b34b81513ee21
SeanPRyan <sean23397@gmail.com>
2017-04-07 18:13:50 +0100

    CSS changes to adhire to static folder for GAE

--- engineapp/register.php
+++ engineapp/register.php
@@ -89,8 +89,8 @@
   <!--REF Coding Cage for the Login and registration page !-->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Registration Page OneStop Shop</title>
-<link href="bootstrap.min.css" rel="stylesheet">
-<link href="business-casual.css" rel="stylesheet">
+<link href="/stylesheets/bootstrap.min.css" rel="stylesheet">
+<link href="/stylesheets/business-casual.css" rel="stylesheet">
 
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
